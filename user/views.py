70f2c2b0adf1cd26e2949976cbc533aa11999b38
niharika1606 <mangalpalliniharika@gmail.com>
2025-04-02 00:33:53 +0530

    correct working

--- user/views.py
+++ user/views.py
@@ -18,8 +18,8 @@ def registeruser(request):
             return redirect('login')
         else:
             # If the form is invalid, render the same page with form errors
-            messages.success(request,f'🔄 Oops! Something went wrong. Give it another shot!')    
-            return render(request, 'uploads/home.html', {'form': form})
+            messages.success(request,f'🔄 Oops! Something went wrong.Give it another Shot!')    
+            return render(request, 'user/register.html', {'form': form})
     else:
      form=UserRegistrationForm()
      return render(request,'user/register.html',{'form':form})
