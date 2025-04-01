cfc86d71ccd378ae63e39132c6c5cf3957556152
niharika1606 <mangalpalliniharika@gmail.com>
2025-04-01 23:21:19 +0530

    correct working

--- project/urls.py
+++ project/urls.py
@@ -47,7 +47,7 @@ urlpatterns = [
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
              template_name='user/password-reset-complete.html'), name='password_reset_complete'),
 
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='user/login.html'), name='password_reset_done'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name='password_reset_confirm'),
 ]
 
