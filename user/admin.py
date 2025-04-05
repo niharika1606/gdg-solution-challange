3399ddc3bed3dc3504f121bd215ecc1d49f22fe2
niharika1606 <mangalpalliniharika@gmail.com>
2025-04-05 19:03:02 +0530

    messages display done

--- user/admin.py
+++ user/admin.py
@@ -3,6 +3,6 @@ from .models import Profile
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'positive', 'neutral', 'negative', 'total_posts')  # Display progress fields
+    list_display = ('user', 'positive', 'neutral', 'negative')  # Display progress fields
     list_filter = ('user',)  # Add filtering options
     search_fields = ('user__username',) 
